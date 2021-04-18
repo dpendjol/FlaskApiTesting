38c7ff3fc0661e2bdb64a45ff5299317a31894e2
Dominique Pendjol <d.pendjol@gmail.com>
2021-04-18 18:33:27 +0200

    Added custom error messages to the integration tests

--- tests/intergration/models/test_item.py
+++ tests/intergration/models/test_item.py
@@ -5,27 +5,32 @@ from models.store import StoreModel
 class ItemTest(BaseTest):
     def test_crud(self):
         with self.app_context():
-            StoreModel('test').save_to_db()
+            store = StoreModel('test')
             item = ItemModel('test', 19.99, 1)
             
-            self.assertIsNone(ItemModel.find_by_name('test'))
+            store.save_to_db()
+            
+            self.assertIsNone(ItemModel.find_by_name('test'),
+                              'Item found where none had to be there')
             
             item.save_to_db()
 
-            self.assertIsNotNone(ItemModel.find_by_name('test'))
+            self.assertIsNotNone(ItemModel.find_by_name('test'),
+                                 'Item not found in the database')
             
             item.delete_from_db()
 
-            self.assertIsNone(ItemModel.find_by_name('test'))
+            self.assertIsNone(ItemModel.find_by_name('test'),
+                              'Item found after deletion, something went wrong')
     
     def test_store_relationship(self):
         with self.app_context():
             store = StoreModel('test')
-            
             item = ItemModel('test', 19.99, 1)
             
             store.save_to_db()
             item.save_to_db()
             
-            self.assertEqual(ItemModel.find_by_name('test').store, store)
+            self.assertEqual(ItemModel.find_by_name('test').store, store,
+                             'Store did not match with freshly inserted store')
             
--- tests/intergration/models/test_store.py
+++ tests/intergration/models/test_store.py
@@ -8,21 +8,25 @@ class StoreTest(BaseTest):
             store = StoreModel('test')
             store.save_to_db()
             
-            self.assertListEqual(store.items.all(), [])
+            self.assertListEqual(store.items.all(), [],
+                                 'List wasn\'t empty where it was supose to be')
     
     def test_crud(self):
         with self.app_context():
             store = StoreModel('test')
             
-            self.assertIsNone(StoreModel.find_by_name('test'))
+            self.assertIsNone(StoreModel.find_by_name('test'),
+                              'Already found a store, is not suppose to be there')
             
             store.save_to_db()
             
-            self.assertIsNotNone(StoreModel.find_by_name('test'))
+            self.assertIsNotNone(StoreModel.find_by_name('test'),
+                                 'Did not found the created store')
             
             store.delete_from_db()
             
-            self.assertIsNone(StoreModel.find_by_name('test'))
+            self.assertIsNone(StoreModel.find_by_name('test'),
+                              'Deletion did not went right, found the store')
         
     def test_store_relationships(self):
         with self.app_context():
@@ -32,8 +36,10 @@ class StoreTest(BaseTest):
             store.save_to_db()
             item.save_to_db()
             
-            self.assertEqual(store.items.count(), 1)
-            self.assertEqual(store.items.first().name, 'testitem')
+            self.assertEqual(store.items.count(), 1,
+                             f'List contains {store.items.count()}, where 1 was expected')
+            self.assertEqual(store.items.first().name, 'testitem',
+                             'List did not contain the item just inserted')
     
     def test_store_json(self):
         with self.app_context():
@@ -44,7 +50,8 @@ class StoreTest(BaseTest):
                 'items': []
             }
             
-            self.assertDictEqual(store.json(), expected)
+            self.assertDictEqual(store.json(), expected,
+                                 'Got another json object back then expected')
             
     def test_store_json_with_items(self):
         with self.app_context():
@@ -60,4 +67,5 @@ class StoreTest(BaseTest):
                            'price': 19.99}]
             }
             
-            self.assertDictEqual(store.json(), expected)
+            self.assertDictEqual(store.json(), expected,
+                                 'Got another json object back then expected')
